dcc7788b7cced5940afe1bc7f16cf98cd0a6f7e4
Armando Castillo <armando.castillo@sundevs.com>
2018-11-01 13:26:40 -0500

    Fixed paths

--- class/Utils.php
+++ class/Utils.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__) . '/' . '../config.php');
-require_once(dirname(__FILE__) . '/' . '../class/Log.php');
+require_once(dirname(__FILE__) . '/../constants.php');
+require_once(dirname(__FILE__) . '/Log.php');
 
 class Utils
 {
