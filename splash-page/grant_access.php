1822b122b713e6f447bfbc25cc895349571eff45
Armando Castillo <armando.castillo@sundevs.com>
2018-11-01 15:34:04 -0500

    Added success screen

--- splash-page/grant_access.php
+++ splash-page/grant_access.php
@@ -4,8 +4,6 @@ $username = 'any_username';
 $password = 'any_password'; //base64_encode(hash_hmac('sha256', $username, $secret, true));
 // Encrypt Password
 $encrypted_password = Utils::encode_password_challenge($password,$node_challenge,$secret);
-$redirectUrl = 'http://' . $controller_ip . ':' . $controller_port . '/logon?username=' . urlencode($username) . '&password=' . urlencode($encrypted_password);
-// Redirect
-Utils::redirect($redirectUrl);
-exit();
+$signedUrl = 'http://' . $controller_ip . ':' . $controller_port . '/logon?username=' . urlencode($username) . '&password=' . urlencode($encrypted_password);
+require_once(dirname(__FILE__).'/success.php');
 ?>
---
+++ splash-page/success.php
@@ -0,0 +1,45 @@
+<?php
+if ($open_external_browser){
+	$destination = 'googlechrome://navigate?url=https://audi-client.sundevs.cloud/#/' . $phone;
+} else {
+	$destination = 'https://audi-client.sundevs.cloud/#/' . $phone;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Codigo de acceso rapido</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+		body {
+			text-align: center;
+			font-family: Verdana, Geneva, sans-serif;
+		}
+
+		p {
+			color: #999;
+			line-height: 20px;
+			font-size: 14px;
+		}
+	</style>
+	<script>
+		function openExternalBrowser(){
+				document.getElementById("connect-button").disabled = true;
+				fetch('<?php echo $signedUrl; ?>');
+				window.open("<?php echo $destination; ?>");
+		};
+	</script>
+</head>
+<body>
+	<h1>Fast pass Audi</h1>
+	<img src="<?php echo $base64QrCode ?>">
+
+	<p>
+		Presenta este código para el ingreso a nuestro pabellón y disfruta de la gran experiencia AUDI que hemos preparado este año para ti. Descubre novedades y conoce más sobre nuestros vehīculos desde tu teléfono móvil.
+    </p>
+    
+	<button id="connect-button" onclick="openExternalBrowser()">Acceder a Internet</button>
+	
+</body>
+</html>
